794494372784bc8221510866594c2f4761c06868
erboro97 <erboro97@gmail.com>
2021-02-13 20:24:17 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GitProjekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitProjekt.dir/main.c.obj"
+  "GitProjekt.exe"
+  "GitProjekt.exe.manifest"
+  "GitProjekt.lib"
+  "GitProjekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GitProjekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
